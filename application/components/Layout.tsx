595cafe8f0cabe99032aaf15a73b9698858eee37
paikwiki <paikwiki@gmail.com>
2021-09-11 16:34:24 +0900

    Menu 동작 데모

--- application/components/Layout.tsx
+++ application/components/Layout.tsx
@@ -1,18 +1,21 @@
-import Menu from './Menu'
-import Head from 'next/head'
-import Nav from './Nav'
+import Menu from "./Menu";
+import Head from "next/head";
+import Nav from "./Nav";
 
-const Layout = ({ children }) => {
+const Layout = ({ children } : any) => {
   return (
     <>
-
-      <Menu />
+      <Menu
+        initIsVisibleArtists={
+          children.type.name === "ArtistNav" ||
+          children.type.name === "artistPage"
+        }
+        initIsVisibleInterviews={children.type.name === "interview"}
+      />
       <Nav />
-      <div>
-        {children}
-      </div>
+      <div>{children}</div>
     </>
-  )
-}
+  );
+};
 
 export default Layout;
--- application/components/Menu.tsx
+++ application/components/Menu.tsx
@@ -1,31 +1,122 @@
-import Link from 'next/link'
+import Link from "next/link";
+import { useState } from "react";
 
-const Menu = () => {
+const artists = [
+  {
+    name: "jin narae",
+    link: "/artists/parkdongjoon",
+  },
+  {
+    name: "park dongjoon",
+    link: "/artists/parkdongjoon",
+  },
+  {
+    name: "seo taelee",
+    link: "/artists/parkdongjoon",
+  },
+  {
+    name: "moon sohyun",
+    link: "/artists/parkdongjoon",
+  },
+  {
+    name: "sucho",
+    link: "/artists/parkdongjoon",
+  },
+];
+
+const interviews = [
+  {
+    name: "jin narae",
+    link: "/interviews/jinnarae",
+  },
+  {
+    name: "park dongjoon",
+    link: "/interviews/parkdongjoon",
+  },
+  {
+    name: "seo taelee",
+    link: "/interviews/seotaelee",
+  },
+  {
+    name: "moon sohyun",
+    link: "/interviews/moonsohyun",
+  },
+];
+
+const Menu = ({ initIsVisibleArtists = false, initIsVisibleInterviews = false }) => {
+  const [isVisibleArtists, setIsVisibleArtists] = useState(
+    initIsVisibleArtists
+  );
+  const [isVisibleInterviews, setIsVisibleInterviews] = useState(
+    initIsVisibleInterviews
+  );
+  const handleClickArtists = () => {
+    setIsVisibleArtists(!isVisibleArtists);
+    setIsVisibleInterviews(false);
+  };
+  const handleClickInterviews = () => {
+    setIsVisibleInterviews(!isVisibleInterviews);
+    setIsVisibleArtists(false);
+  };
+  const handleClickOthers = () => {
+    setIsVisibleArtists(false);
+    setIsVisibleInterviews(false);
+  }
+
   return (
-    <nav>
+    <nav style={{ border: "1px solid #ddd" }}>
       <ul>
-        <li>
-          <Link href="/"><a>Home</a></Link>
+        <li onClick={handleClickOthers}>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
         </li>
         <li>
-          <Link href="/artists"><a>Artists</a></Link>
+          <span onClick={handleClickArtists}>Artists</span>
+          {isVisibleArtists && (
+            <ul>
+              {artists.map((artist) => (
+                <li key={artist.name}>
+                  <Link href={artist.link}>
+                    <a>{artist.name}</a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
         </li>
         <li>
-          <Link href="/interview"><a>Interview</a></Link>
+          <span onClick={handleClickInterviews}>Interview</span>
+          {isVisibleInterviews && (
+            <ul>
+              {interviews.map((interview) => (
+                <li key={interview.name}>
+                  <Link href={interview.link}>
+                    <a>{interview.name}</a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
         </li>
-        <li>
-          <Link href="/exhibition"><a>Exhibition</a></Link>
+        <li onClick={handleClickOthers}>
+          <Link href="/exhibition">
+            <a>Exhibition</a>
+          </Link>
         </li>
-        <li>
-          <Link href="/texts"><a>Texts</a></Link>
+        <li onClick={handleClickOthers}>
+          <Link href="/texts">
+            <a>Texts</a>
+          </Link>
         </li>
-        <li>
-          <Link href="/networking"><a>Networking</a></Link>
+        <li onClick={handleClickOthers}>
+          <Link href="/networking">
+            <a>Networking</a>
+          </Link>
         </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-
-export default Menu
+export default Menu;
