8ed2c71cad24b36c69f31e148e442f106b0b947a
Sungyong Cho <to.sungyongcho@gmail.com>
2021-09-22 12:26:03 +0900

    viewport test 2

--- application/pages/_document.tsx
+++ application/pages/_document.tsx
@@ -36,31 +36,6 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <meta charSet="utf-8" />
-          {/* TODO: og 기본값 세팅 */}
-          <link rel="shortcut icon" href="/favicon.svg" type="image/svg" />
-          <meta property="og:title" content="" />
-          <meta property="og:image" content="" />
-          <meta property="og:description" content="" />
-          <meta property="og:url" content="//" />
-          <meta name="description" content="" />
-          <meta name="keywords" content="" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700;800;900&display=swap"
-            rel="preload"
-            as="style"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700;800;900&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
         <body>
           <Main />
           <NextScript />
