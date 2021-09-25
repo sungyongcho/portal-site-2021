045f095d607038c1a420c5ec6d7f7518f52fba11
Sungyong Cho <to.sungyongcho@gmail.com>
2021-09-25 19:01:52 +0900

    modal wip

--- application/components/member-worklist.tsx
+++ application/components/member-worklist.tsx
@@ -5,17 +5,32 @@ import Modal from 'react-modal';
 import { media } from '../styles/theme'
 
 type Props = {
-  workList: string[];
+  workList: [{
+    address: string,
+    workTitle: string,
+    workYear: number,
+    workMedia: string,
+    workSize: string
+  }];
 }
 
 const MemberWorklist = ({ workList }: Props) => {
 
   const [modalIsOpen, setIsOpen] = useState(false);
   const [imageAddress, setImageAddress] = useState('');
+  const [captionTitle, setCaptionTitle] = useState('');
+  const [captionYear, setCaptionYear] = useState(0);
+  const [captionMedia, setCaptionMedia] = useState('');
+  const [captionSize, setCaptionSize] = useState('');
+
 
   const openModal = (e, src) => {
     setIsOpen(true);
-    setImageAddress(src);
+    setImageAddress(src.address);
+    setCaptionTitle(src.workTitle);
+    setCaptionYear(src.workYear);
+    setCaptionMedia(src.workMedia);
+    setCaptionSize(src.workSize);
   }
 
   function afterOpenModal() {
@@ -25,6 +40,10 @@ const MemberWorklist = ({ workList }: Props) => {
   function closeModal() {
     setIsOpen(false);
     setImageAddress('');
+    setCaptionTitle('');
+    setCaptionYear(0);
+    setCaptionMedia('');
+    setCaptionSize('');
   }
 
   return (
@@ -34,17 +53,25 @@ const MemberWorklist = ({ workList }: Props) => {
         <Image onClick={(e) => {
           openModal(e, workList)
         }}
-          src={`${workList}`} width='200' height='400px' alt="" />
+          src={`${workList.address}`} width='200' height='400px' alt="" />
       ))}
       {/* for modal popup*/}
       {
         modalIsOpen && <Modal
+          className="_"
+          overlayClassName="_"
           isOpen={modalIsOpen}
           onRequestClose={closeModal}
+          contentElemet={(props, children) => <ModalStyle {...props}>{children}</ModalStyle>}
+          overlayElement={(props, contentElement) => <OverlayStyle {...props}>{contentElement}</OverlayStyle>}
         >
           <Image onClick={closeModal}
             src={imageAddress} layout="fill"
-            objectFit="contain" alt="" />
+            objectFit="contain" alt=""></Image>
+          {captionTitle !== '' && <div>{captionTitle}</div>}
+          {captionYear !== 0 && <div>{captionYear}</div>}
+          {captionMedia !== '' && <div>{captionMedia}</div>}
+          {captionSize !== '' && <div>{captionSize}</div>}
         </Modal>
       }
 
@@ -61,4 +88,22 @@ const WorklistLayout = styled.div`
       }
 
 `
+
+const ModalStyle = styled.div`
+  width:1em;
+  background-color: blue;
+`;
+
+
+const OverlayStyle = styled.div`
+  background-color: black;
+`;
+
+const ImageFrame = styled(Image)`
+  display:inline-block;
+  width:15%;
+  height:10%;
+  background-color: gray;
+`;
+
 export default MemberWorklist
