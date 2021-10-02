5ac2509d93af1702a76dbd6c72ac708be85cc2e5
paikwiki <paikwiki@gmail.com>
2021-10-02 11:24:26 +0900

    작품목록 드래그할 때 modal이 활성화되는 오류 수정

--- application/components/member-worklist.tsx
+++ application/components/member-worklist.tsx
@@ -1,8 +1,8 @@
-import Image from 'next/image'
-import styled from 'styled-components'
-import { useState } from 'react';
-import Modal from './Modal';
-import { media } from '../styles/theme'
+import Image from "next/image";
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import Modal from "./Modal";
+import { media } from "../styles/theme";
 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -10,14 +10,16 @@ import "slick-carousel/slick/slick-theme.css";
 import { useWindowSize } from "../hooks/useWindowSize";
 
 type Props = {
-  workList: [{
-    address: string,
-    workTitle: string,
-    workYear: number,
-    workMedia: string,
-    workSize: string
-  }];
-}
+  workList: [
+    {
+      address: string;
+      workTitle: string;
+      workYear: number;
+      workMedia: string;
+      workSize: string;
+    }
+  ];
+};
 
 const mobileSetting = {
   className: "slider-mobile variable-width",
@@ -27,7 +29,7 @@ const mobileSetting = {
   centerMode: true,
   slidesToShow: 1,
   slidesToScroll: 1,
-  variableWidth: true
+  variableWidth: true,
 };
 
 const tabletSetting = {
@@ -36,7 +38,7 @@ const tabletSetting = {
   slidesToShow: 3,
   slidesToScroll: 1,
   infinite: false,
-  variableWidth: true
+  variableWidth: true,
 };
 
 /**
@@ -45,140 +47,175 @@ const tabletSetting = {
  */
 const MoveDragThreshold = 10;
 
-
-
-
 const MemberWorklist = ({ workList }: Props) => {
-
   const { height, width } = useWindowSize();
 
-  const [modalIsOpen, setIsOpen] = useState(false);
-  const [imageAddress, setImageAddress] = useState('');
-  const [captionTitle, setCaptionTitle] = useState('');
-  const [captionYear, setCaptionYear] = useState(0);
-  const [captionMedia, setCaptionMedia] = useState('');
-  const [captionSize, setCaptionSize] = useState('');
+  // const [isOpen, setIsOpen] = useState(false);
+  // const [imageAddress, setImageAddress] = useState("");
+  // const [captionTitle, setCaptionTitle] = useState("");
+  // const [captionYear, setCaptionYear] = useState(0);
+  // const [captionMedia, setCaptionMedia] = useState("");
+  // const [captionSize, setCaptionSize] = useState("");
+  const [modal, setModal] = useState({
+    imageAddress: "",
+    captionTitle: "",
+    captionYear: 0,
+    captionMedia: "",
+    captionSize: "",
+    isOpen: false,
+  });
+  const [clickedPosision, setClickedPosisionX] =
+    useState<{ x: number; y: number }>(null);
 
   const [showModal, setShowModal] = useState(false);
 
-  const openModal = (e, src) => {
-    setIsOpen(true);
-    setImageAddress(src.address);
-    setCaptionTitle(src.workTitle);
-    setCaptionYear(src.workYear);
-    setCaptionMedia(src.workMedia);
-    setCaptionSize(src.workSize);
-    setShowModal(true);
-  }
+  useEffect(() => {
+    console.log("useEffect");
+    modal.isOpen ? setShowModal(true) : setShowModal(false);
+  }, [modal]);
+
+  const handleOnMouseDown = (e: React.MouseEvent<HTMLElement>) => {
+    console.log("handleOnMouseDown");
+    setClickedPosisionX({
+      x: e.clientX,
+      y: e.clientY,
+    });
+    e.preventDefault();
+  };
+
+  const handleOnMouseUp = (e: React.MouseEvent<HTMLElement>, work) => {
+    console.log("handleOnMouseUp", clickedPosision);
+    if (
+      Math.abs(clickedPosision.x - e.clientX) >= MoveDragThreshold ||
+      Math.abs(clickedPosision.y - e.clientY) >= MoveDragThreshold
+    )
+      return;
+    setModal({
+      imageAddress: work.address,
+      captionTitle: work.workTitle,
+      captionYear: work.workYear,
+      captionMedia: work.workMedia,
+      captionSize: work.workSize,
+      isOpen: true,
+    });
+  };
 
   function afterOpenModal() {
     // references are now sync'd and can be accessed.
   }
 
-  function closeModal() {
-    setIsOpen(false);
-    setShowModal(false);
-    setImageAddress('');
-    setCaptionTitle('');
-    setCaptionYear(0);
-    setCaptionMedia('');
-    setCaptionSize('');
+  function handleClickModal() {
+    setModal({
+      imageAddress: "",
+      captionTitle: "",
+      captionYear: 0,
+      captionMedia: "",
+      captionSize: "",
+      isOpen: false,
+    })
   }
-
 
   return (
     <>
-      {workList && (width > 767 ?
-        (
+      {workList &&
+        (width > 767 ? (
           <TabletFrame>
             <TabletImage>
-
               <Slider {...tabletSetting}>
-                {workList.map((workList) => (
-                  <>
+                {workList.map((work) => (
+                  <div
+                    key={work.workTitle}
+                  >
                     <img
-                      alt="" onClick={(e) => {
-                        openModal(e, workList)
-                      }}
-                      src={`${workList.address}`} />
-                  </>
+                      alt="work description"
+                      onMouseDown={handleOnMouseDown}
+                      onMouseUp={(e) => handleOnMouseUp(e, work)}
+                      src={`${work.address}`}
+                    />
+                  </div>
                 ))}
                 <div />
                 <div />
               </Slider>
             </TabletImage>
           </TabletFrame>
-        )
-        :
-        (
+        ) : (
           <MobileFrame>
             <MobileImage>
               <Slider {...mobileSetting}>
-                {workList.map((workList) => (
-                  <>
+                {workList.map((work) => (
+                  <div
+                    key={work.workTitle}
+                  >
                     <img
-                      alt="" onClick={(e) => {
-                        openModal(e, workList)
-                      }}
-                      src={`${workList.address}`} />
-                  </>
+                      alt="work description"
+                      onMouseDown={handleOnMouseDown}
+                      onMouseUp={(e) => handleOnMouseUp(e, work)}
+                      src={`${work.address}`}
+                    />
+                  </div>
                 ))}
               </Slider>
             </MobileImage>
-          </MobileFrame>))}
-      {
-        modalIsOpen && <Modal
+          </MobileFrame>
+        ))}
+      {modal.isOpen && (
+        <Modal
           onClose={() => setShowModal(false)}
-          show={showModal} title={captionTitle}
+          show={showModal}
+          title={modal.captionTitle}
         >
-          {
-            width > 767 ?
-              (
-                <div className="unset-img-desktop">
-                  <img className="custom-img-desktop" onClick={closeModal}
-                    src={imageAddress}
-                    alt="" />
-                </div>
-              ) :
-              (
-                <div className="unset-img-mobile">
-                  <img className="custom-img-mobile" onClick={closeModal}
-                    src={imageAddress}
-                    alt="" />
-                </div>
-              )
-          }
+          {width > 767 ? (
+            <div className="unset-img-desktop">
+              <img
+                className="custom-img-desktop"
+                onClick={handleClickModal}
+                src={modal.imageAddress}
+                alt=""
+              />
+            </div>
+          ) : (
+            <div className="unset-img-mobile">
+              <img
+                className="custom-img-mobile"
+                onClick={handleClickModal}
+                src={modal.imageAddress}
+                alt=""
+              />
+            </div>
+          )}
           <br />
           <CaptionStyle>
-            <TitleStyle>{captionTitle !== '' && <div>{captionTitle}</div>}</TitleStyle>
-            {captionYear !== 0 && <div>{captionYear}</div>}
-            {captionMedia !== '' && <div>{captionMedia}</div>}
-            {captionSize !== '' && <div>{captionSize}</div>}</CaptionStyle>
+            <TitleStyle>
+              {modal.captionTitle !== "" && <div>{modal.captionTitle}</div>}
+            </TitleStyle>
+            {modal.captionYear !== 0 && <div>{modal.captionYear}</div>}
+            {modal.captionMedia !== "" && <div>{modal.captionMedia}</div>}
+            {modal.captionSize !== "" && <div>{modal.captionSize}</div>}
+          </CaptionStyle>
         </Modal>
-      }
+      )}
     </>
-  )
-}
+  );
+};
 
 const DesktopContainer = styled.div`
-    width:110%;
-    height: 30vh;
-    overflow:hidden;
+  width: 110%;
+  height: 30vh;
+  overflow: hidden;
 `;
 
-
 const StyledSlider = styled(Slider)`
-  .slick-slide div{
+  .slick-slide div {
     outline: none;
   }
 `;
-
 
 const CaptionStyle = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  ${media.desktop}{
+  ${media.desktop} {
     font-size: 1.5em;
     line-height: 1.2;
   }
@@ -203,11 +240,11 @@ const MobileFrame = styled.div`
 `;
 
 const MobileContainer = styled.div`
-  background-color:transparent;
+  background-color: transparent;
 `;
 
 const MobileImage = styled.div`
-  width:auto;
+  width: auto;
 `;
 
 const TabletFrame = styled.div`
@@ -217,17 +254,14 @@ const TabletFrame = styled.div`
   width: 200%;
   height: 20vh;
   overflow: hidden;
-  ${media.desktop}
-  {
+  ${media.desktop} {
     height: 25vh;
   }
 `;
 
 const TabletImage = styled.div`
-  width:auto;
+  width: auto;
 `;
 
-
-
 // https://github.com/akiran/react-slick/issues/848
-export default MemberWorklist
+export default MemberWorklist;
